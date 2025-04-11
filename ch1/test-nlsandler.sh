7c133c0b6e485dfa7c421d30bbda15eb1ce1ceea
Jason Pepas <jasonpepas@gmail.com>
2025-04-11 17:43:04 -0500

    git clone the test suite if missing

--- ch1/test-nlsandler.sh
+++ ch1/test-nlsandler.sh
@@ -10,17 +10,30 @@
 
 set -e -o pipefail
 
+testpath="$HOME/github/nlsandler/writing-a-c-compiler-tests"
+if ! test -e "$testpath" ; then
+    echo "Downloading nlsandler/writing-a-c-compiler-tests" >&2
+    mkdir -p ~/github/nlsandler
+    cd ~/github/nlsandler
+    git clone git@github.com:nlsandler/writing-a-c-compiler-tests
+    cd -
+fi
+export PATH="$PATH:$testpath"
+
 ./build.sh
 
-export PATH="$PATH:$HOME/github/nlsandler/writing-a-c-compiler-tests"
-
 flags="--verbose --verbose --verbose --failfast"
+
+ch=$1
+if test -z "$ch" ; then
+    ch=1
+fi
 
 if test -n "$2" ; then
     # note: --stage is one of lex,parse,validate,tacky,codegen,run
     set -x
-    test_compiler $flags jpcc --chapter $1 --stage $2
+    test_compiler $flags jpcc --chapter $ch --stage $2
 else
     set -x
-    test_compiler $flags jpcc --chapter $1
+    test_compiler $flags jpcc --chapter $ch
 fi
